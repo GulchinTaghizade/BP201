cba415a41f27ace19769a09ca0be3724505fcb8f
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-04-11 19:07:26 +0400

    Add files via upload

---
+++ 04.11.2022/04.11.2022/Enums/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._11._2022.Enums
+{
+    enum Categories
+    {
+        Programming=1,
+        Design,
+        SystemAdministration
+   
+}
+}
