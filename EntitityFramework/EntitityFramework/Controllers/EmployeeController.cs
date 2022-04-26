338f2ecb5f7d7cf067d843f23127c99f26640cca
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-04-26 18:21:23 +0400

    uploaded task

---
+++ EntitityFramework/EntitityFramework/Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using EntitityFramework.Dataaccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntitityFramework.Controllers
+{
+    class EmployeeController
+    {
+        private readonly AppDBContext _context;
+
+        public EmployeeController()
+        {
+            _context = new AppDBContext();
+        }
+
+        public void GetEmployeeById(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            Console.WriteLine(employee.Fullname); 
+            
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
+
+
+        }
+
+        public void AddEmployee(string fullname)
+        {
+            Employee emp1 = new Employee {Fullname=fullname};
+            _context.Employees.Add(emp1);
+            _context.SaveChanges();
+            Console.WriteLine($"{fullname} was added to Employees table");
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            Employee emp1 = _context.Employees.Find(id);
+            _context.Employees.Remove(emp1);
+            _context.SaveChanges();
+            Console.WriteLine($"{id}th data was deleted");
+        }
+
+        public void FilterByName(string search)
+        {
+            List<Employee> emplist = _context.Employees.ToList();
+
+            foreach (var item in emplist)
+            {
+                if (item.Fullname.Contains(search))
+                {
+                    Console.WriteLine(item.Fullname);
+                }  
+            }
+
+            
+        }
+    }
+
+   
+
+}
---
+++ EntitityFramework/EntitityFramework/Dataaccess/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntitityFramework.Dataaccess
+{
+    class AppDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-6EQ5FGI; Database=CompanyDB;Integrated Security=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
