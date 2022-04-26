3c31deab06eebfc151481e8d13939352af27a76e
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-04-26 18:22:11 +0400

    Add files via upload

---
+++ EntitityFramework/EntitityFramework/Program.cs
@@ -0,0 +1,23 @@
+﻿using EntitityFramework.Controllers;
+using System;
+
+namespace EntitityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeController emp1 = new EmployeeController();
+            // emp1.GetEmployeeById(1);
+
+            //foreach (var item in emp1.GetAllEmployees())
+            //{
+            //    Console.WriteLine(item.Fullname);
+            //}
+
+            //emp1.AddEmployee("Eliyev Samir");
+            //emp1.DeleteEmployee(4);
+            //emp1.FilterByName("i");
+        }
+    }
+}
